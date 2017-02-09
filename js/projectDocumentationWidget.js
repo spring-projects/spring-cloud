796733cda60c486dabfa98a06b473add54258d38
Borja Lafuente Romero <borja.lafuente.romero@ericsson.com>
2017-02-09 17:53:59 +0100

    Dalston is built on boot 1.5.1.BUILD-SNAPSHOT

--- js/projectDocumentationWidget.js
+++ js/projectDocumentationWidget.js
@@ -150,7 +150,7 @@ Spring.SnippetView = Backbone.View.extend({
   },
 
   render: function () {
-    this.model.bootVersion = this.model.version.indexOf("Dalston")!=-1 ? "1.5.0.BUILD-SNAPSHOT" :
+    this.model.bootVersion = this.model.version.indexOf("Dalston")!=-1 ? "1.5.1.BUILD-SNAPSHOT" :
                              this.model.version.indexOf("Camden")!=-1 ? "1.4.4.RELEASE" :
                              this.model.version.indexOf("Brixton")!=-1 ? "1.3.8.RELEASE" :
                              "1.2.8.RELEASE";
